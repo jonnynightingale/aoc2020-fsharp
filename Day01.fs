9fb447b321f1b2c372e244d95e280dbd08c3221f
Jonny Bolton <jonnybolton@gmail.com>
2021-02-03 15:09:35 +0000

    Day 1 complete

---
+++ Day01.fs
@@ -0,0 +1,31 @@
+﻿module Day01
+
+/// Searches for the first pair of numbers in a collection that sum to a given
+/// target. If found, this returns an option containing the product of those two
+/// numbers.
+let rec private ProductPair target numbers =
+    match numbers with
+    | n::ns ->
+        let m = target - n
+        if List.contains m ns
+        then Some(n * m)
+        else ProductPair target ns
+    | [] -> None
+
+/// Searches for the first triplet of numbers in a collection that sum to a
+/// given target. If found, this returns an option containing the product of
+/// those three numbers.
+let rec private ProductTriplet target numbers =
+    match numbers with
+    | n::ns ->
+        match ProductPair (target - n) ns with
+        | Some(pairProduct) -> Some(n * pairProduct)
+        | None -> ProductTriplet target ns
+    | [] -> None
+
+let solve (input : string array) =
+    let numbers = input |> Array.map int |> Array.toList
+    (
+        ProductPair 2020 numbers |> Option.get,
+        ProductTriplet 2020 numbers |> Option.get
+    )
---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿module Program
+
+open System.IO
+
+type Problem = {
+    dayNumber : int
+    filename : string
+    solve : string array -> int * int
+}
+
+let problems = [
+    { dayNumber = 1; filename = @"../../../input/day01.txt"; solve = Day01.solve }
+]
+
+let PrintSolution (dayNumber : int) (solution : int * int) =
+    printfn "Day %i" dayNumber
+    fst solution |> printfn "  Part one: %i"
+    snd solution |> printfn "  Part two: %i"
+
+let Solve problem = File.ReadAllLines problem.filename |> problem.solve
+
+[<EntryPoint>]
+let main argv =
+    problems
+    |> List.map (fun problem -> problem.dayNumber, Solve problem)
+    |> List.map (fun x -> PrintSolution (fst x) (snd x))
+    |> ignore
+    0
